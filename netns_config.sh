b1b36f3ae660179eee92711d11a566b8779955f5
sergey <sergey@marrow.pro>
2017-05-17 17:09:55 +0300

    netns_config updated

--- netns_config.sh
+++ netns_config.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+#Предварительно требуется включить маршрутизацию:
+# sudo su
+# echo 1 > /proc/sys/net/ipv4/ip_forward
+
 case $1 in
-setup)
+'setup')
 
 #Настройка netns для измерений.
 
@@ -23,18 +27,24 @@ ip link set nteth1 netns NetTestClient
 #Поднимаем все созданные интерфейсы
 
 ifconfig veth0 192.168.2.1/24 up
-ip netns exec NetTestServer ifconfig nteth0 192.168.3.1/24 up
-ifconfig veth1 192.168.2.2/24 up
+ip netns exec NetTestServer ifconfig nteth0 192.168.2.2/24 up
+ifconfig veth1 192.168.3.1/24 up
 ip netns exec NetTestClient ifconfig nteth1 192.168.3.2/24 up
+
+#Прописываем маршруты из стеков NetTestServer и NetTestClient
+ip netns exec NetTestServer ip route add 192.168.3.0/24 via 192.168.2.1
+ip netns exec NetTestClient ip route add 192.168.2.0/24 via 192.168.3.1
+
+#Прописываем маршруты для перенаправления потоков из NetTestServer и NetTestClient во внешнюю сеть
 ;;
 
-remove)
+'remove')
 ip netns del NetTestServer
 ip netns del NetTestClient
 ;;
 
 *)
-echo "неверный параметр"
+echo "Usage: $0 { setup | remove }"
 
 esac
 
